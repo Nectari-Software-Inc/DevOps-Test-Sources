e8b7f434de7b3fcaa62a1fac59d05079ea1caed6
Bilal <12447356+Bilal-Nectari@users.noreply.github.com>
2022-10-11 10:41:15 -0400

    Update Question_2.ps1

--- Question 2/Question_2.ps1
+++ Question 2/Question_2.ps1
@@ -1,2 +1,5 @@
+# If you don't have access to powershell on your machine, please do not hesitate to fetch the docker image to do this exercice.
+# The code below is an example on how to fetch the json from the endpoint.
+# Please provide the proper code to have a display that matches the question.
 $response1 = Invoke-WebRequest -URI http://jsonplaceholder.typicode.com/albums
 $response2 = Invoke-WebRequest -URI http://jsonplaceholder.typicode.com/photos
